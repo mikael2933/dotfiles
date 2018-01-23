2faf52549d6d39928cfec366865c704a8cc54592
Mikael <prizelive@live.dk>
2018-01-23 10:05:29 +0100

    New changes idk man

---
+++ .emacs.d/config.el
@@ -0,0 +1,20 @@
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (pallet package-build shut-up epl git commander f dash s))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'ox-beamer)
+
+(setq org-support-shift-select t)
---
+++ .emacs.d/init.el
@@ -0,0 +1,27 @@
+(package-initialize)
+
+;; Cask and pallet - Package Management
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+
+(org-babel-load-file "~/.emacs.d/config.org")
+;; Beamer org-export
+;;(require 'ox-beamer)
+
+;;(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(inhibit-startup-screen t)
+;; '(package-selected-packages
+;;   (quote
+;;    (pallet package-build shut-up epl git commander f dash s))))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; )
