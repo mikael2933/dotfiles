8f0f178a881a56c840b45d219b568d3428364624
Mikael <prizelive@live.dk>
2018-02-15 19:54:40 +0100

    Added Smex, Haskell-mode and Intero

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,9 +12,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (custom-manoj-dark)))
+ '(custom-safe-themes
+   (quote
+    ("3e909a9928f6858f37d76ab0bfb660c49a9b820f77cd469c64b042708ed80f71" default)))
+ '(haskell-mode-hook nil)
+ '(haskell-stylish-on-save t)
+ '(haskell-tags-on-save t)
+ '(intero-global-mode t nil (intero))
  '(package-selected-packages
    (quote
-    (multiple-cursors buffer-move ## god-mode haskell-mode counsel rainbow-mode yasnippet-snippets ranger rainbow-delimiters pallet org-bullets ob-sagemath magit linum-relative helm-sage flyspell-correct-ivy exwm doom-themes dashboard color-theme-sanityinc-tomorrow cdlatex beacon auto-complete-sage))))
+    (intero smex multiple-cursors buffer-move ## god-mode haskell-mode counsel rainbow-mode yasnippet-snippets ranger rainbow-delimiters pallet org-bullets ob-sagemath magit linum-relative helm-sage flyspell-correct-ivy exwm doom-themes dashboard color-theme-sanityinc-tomorrow cdlatex beacon auto-complete-sage))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
