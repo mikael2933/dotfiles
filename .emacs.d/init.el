51fd106026a31c329bfb64aa03fa5df722e264da
Mikael <prizelive@live.dk>
2018-03-07 18:09:39 +0100

    Updated Packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,11 +34,11 @@
  '(max-specpdl-size 100000)
  '(package-selected-packages
    (quote
-    (ghub helm helm-core ivy yasnippet smartparens powerline ibuffer-git dumb-jump counsel-projectile projectile git-gutter iedit expand-region aggressive-indent undo-tree ox-reveal ace-window swiper-helm atomic-chrome intero smex multiple-cursors buffer-move ## god-mode haskell-mode counsel rainbow-mode yasnippet-snippets ranger rainbow-delimiters pallet org-bullets ob-sagemath magit linum-relative helm-sage flyspell-correct-ivy exwm doom-themes dashboard color-theme-sanityinc-tomorrow cdlatex beacon auto-complete-sage)))
+    (git-timemachine ghub helm helm-core ivy yasnippet smartparens powerline ibuffer-git dumb-jump counsel-projectile projectile git-gutter iedit expand-region aggressive-indent undo-tree ox-reveal ace-window swiper-helm atomic-chrome intero smex multiple-cursors buffer-move ## god-mode haskell-mode counsel rainbow-mode yasnippet-snippets ranger rainbow-delimiters pallet org-bullets ob-sagemath magit linum-relative helm-sage flyspell-correct-ivy exwm doom-themes dashboard color-theme-sanityinc-tomorrow cdlatex beacon auto-complete-sage)))
  '(projectile-completion-system (quote ivy))
  '(ring-bell-function (quote ignore))
- '(vc-follow-symlinks t)
  '(show-smartparens-global-mode t)
+ '(vc-follow-symlinks t)
  '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
