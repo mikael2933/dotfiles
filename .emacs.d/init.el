c7faca0b1d2454607df5b2096bbf5cb0d122543d
Mikael <prizelive@live.dk>
2018-04-07 13:12:51 +0200

    Fixed haskell indentation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,6 +29,8 @@
  '(haskell-indentation-layout-offset 4)
  '(haskell-indentation-left-offset 4)
  '(haskell-indentation-starter-offset 4)
+ '(haskell-indentation-where-post-offset 4)
+ '(haskell-indentation-where-pre-offset 4)
  '(haskell-mode-hook nil)
  '(haskell-stylish-on-save t)
  '(haskell-tags-on-save t)
