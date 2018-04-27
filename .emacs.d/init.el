97f25a94de7af01f86e6ecbe724f124057ff0931
Mikael <prizelive@live.dk>
2018-04-27 17:47:11 +0200

    Disable auto anzu to mode line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,6 +26,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(anzu-cons-mode-line-p nil)
  '(company-ghc-show-info t)
  '(counsel-projectile-mode t nil (counsel-projectile))
  '(custom-enabled-themes (quote (custom-manoj-dark)))
