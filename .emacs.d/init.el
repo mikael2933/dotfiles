31f0a9a388acd62c7870ecb563287f9f5edffecf
Mikael <prizelive@live.dk>
2018-08-13 16:03:42 +0000

    Fixed crash on startup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,7 +27,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(anzu-cons-mode-line-p nil)
- '(counsel-projectile-mode t nil (counsel-projectile))
  '(custom-enabled-themes (quote (custom-leuven)))
  '(custom-safe-themes
    (quote
