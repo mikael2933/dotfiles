89df8c551915ec0aa9a80d43acd8866fa3f19f1d
Mikael <prizelive@live.dk>
2019-09-01 14:21:18 +0200

    feat(doom): Added helm-locate binding

--- doom-emacs-private/+bindings.el
+++ doom-emacs-private/+bindings.el
@@ -4,6 +4,8 @@
       :n "C-<next>" #'centaur-tabs-forward
 
       :leader
+      :desc "Locate file" "l" #'helm-locate
+
       (:prefix-map ("b" . "buffer")
         :desc "Previous buffer"             "<left>"   #'previous-buffer
         :desc "Next buffer"                 "<right>"   #'next-buffer))
