973af5355daf086488676761d23b162a2d094a76
Mikael <prizelive@live.dk>
2019-09-01 13:57:26 +0200

    feat(doom): Added projectile search path

--- doom-emacs-private/config.el
+++ doom-emacs-private/config.el
@@ -8,6 +8,8 @@
 ;; Temporary fix before Emacs 26.3
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
+(setq projectile-project-search-path '("~/Projects/"))
+
 ;; Helm tramp
 (use-package! helm-tramp
   :config
