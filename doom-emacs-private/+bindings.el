71930166c36cfc906957c8607c79f73fa9d9e8ad
Mikael <prizelive@live.dk>
2019-09-01 13:53:41 +0200

    feat(doom): Added centaur bindings

--- doom-emacs-private/+bindings.el
+++ doom-emacs-private/+bindings.el
@@ -1,6 +1,9 @@
 ;;; ~/.config/doom/+bindings.el -*- lexical-binding: t; -*-
 
-(map! :leader
+(map! :n "C-<prior>" #'centaur-tabs-backward
+      :n "C-<next>" #'centaur-tabs-forward
+
+      :leader
       (:prefix-map ("b" . "buffer")
         :desc "Previous buffer"             "<left>"   #'previous-buffer
         :desc "Next buffer"                 "<right>"   #'next-buffer))
