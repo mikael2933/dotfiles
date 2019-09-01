6eadb4b341f6b728e65302b46ebc027ebfae2b7f
Mikael <prizelive@live.dk>
2019-09-01 13:54:13 +0200

    feat(doom): Added new font sizes

--- doom-emacs-private/+ui.el
+++ doom-emacs-private/+ui.el
@@ -6,4 +6,6 @@
 ;; use relative line numbers
 (setq display-line-numbers-type 'relative)
 
-(setq doom-font (font-spec :family "Fira Mono"))
+(setq doom-font (font-spec :family "Fira Mono")
+      doom-big-font (font-spec :family "Fira Mono" :size 36)
+      doom-variable-pitch-font (font-spec :family "Noto Sans" :size 20))
